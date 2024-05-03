fa0ff3581335707ce5ba8e90e94ce9a6ed0fd289
AlanDAmario <vega93@live.it>
2024-05-03 11:50:50 +0200

    [upd] fix

--- jssnaks2/js/main.js
+++ jssnaks2/js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 //CRAZIONE VARIABILI DELLE PAROLE TRAMITE PROMPT
 const word1 = prompt('inserisci parola 1');
+console.log(word1);
 const word2 = prompt('inserisci parola 2');
+console.log(word2);
 
 //CRAZIONE VARIABILI LUNGHEZZE 
 const wordLen1 = word1.length;
@@ -13,7 +15,4 @@ if (wordLen1 > wordLen2) {
     console.log(wordLen1, 'stampo parola 1');
 } else if (wordLen2 > wordLen1) {
     console.log(wordLen2, 'stampo parola 2');
-} else (wordLen1 === wordLen2){
-    console.log('le parole sono della stessa lunghezza');
-}
-
+} 
